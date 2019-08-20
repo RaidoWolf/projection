42781f3ac4bfc55736d9e5f3cada3cf8da7a7d3d
Alexander Barber <alex@dangerzonegames.com>
2019-08-19 20:04:20 -0600

    :tada: creating mix project

---
+++ lib/quaternion.ex
@@ -0,0 +1,18 @@
+defmodule Quaternion do
+  @moduledoc """
+  Documentation for Quaternion.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Quaternion.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/quaternion_test.exs
@@ -0,0 +1,8 @@
+defmodule QuaternionTest do
+  use ExUnit.Case
+  doctest Quaternion
+
+  test "greets the world" do
+    assert Quaternion.hello() == :world
+  end
+end
